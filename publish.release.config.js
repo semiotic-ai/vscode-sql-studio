ea0f95ce6fb7c144887bda1d757c07b7f0af7c2b
Anirudh <anirudh@semiotic.ai>
2024-02-02 13:58:37 -0800

    chore: add release config

---
+++ publish.release.config.js
@@ -0,0 +1,24 @@
+// publish.release.config.js
+const common = require("./common.release.config.js");
+
+module.exports = {
+  ...common,
+  plugins: [
+    ...common.plugins,
+    [
+      "semantic-release-vsce",
+      {
+        packageVsix: false,
+        publishPackagePath: "*/*.vsix",
+      },
+    ],
+    "@semantic-release/git",
+    [
+      "@semantic-release/github",
+      {
+        assets: "*/*.vsix",
+        addReleases: "bottom",
+      },
+    ],
+  ],
+};

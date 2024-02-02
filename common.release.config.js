4a5bac220203952072da3859bab864a46ec06a40
Anirudh <anirudh@semiotic.ai>
2024-02-02 14:52:17 -0800

    chore: apply prettier

--- common.release.config.js
+++ common.release.config.js
@@ -1,55 +1,54 @@
 // common.release.config.js
 module.exports = {
-    plugins: [
-      [
-        "@semantic-release/commit-analyzer",
-        {
-          releaseRules: [
-            {
-              type: "perf",
-              release: "patch",
-            },
-          ],
-        },
-      ],
-      [
-        "@semantic-release/release-notes-generator",
-        {
-          presetConfig: {
-            types: [
-              {
-                type: "feat",
-                section: "Features",
-              },
-              {
-                type: "fix",
-                section: "Bug Fixes",
-              },
-              {
-                type: "perf",
-                section: "Performance Improvements",
-              },
-              {
-                type: "revert",
-                section: "Reverts",
-              },
-              {
-                type: "build",
-                scope: "deps",
-                section: "Dependencies",
-              },
-            ],
-          },
-        },
-      ],
-      [
-        "@semantic-release/changelog", 
-        {
-            "changelogTitle": "# Changelog"
-        },
-      ],
-      
-    ],
-    branches: ["main"],
-    preset: "conventionalcommits",
-  };
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				releaseRules: [
+					{
+						type: 'perf',
+						release: 'patch'
+					}
+				]
+			}
+		],
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				presetConfig: {
+					types: [
+						{
+							type: 'feat',
+							section: 'Features'
+						},
+						{
+							type: 'fix',
+							section: 'Bug Fixes'
+						},
+						{
+							type: 'perf',
+							section: 'Performance Improvements'
+						},
+						{
+							type: 'revert',
+							section: 'Reverts'
+						},
+						{
+							type: 'build',
+							scope: 'deps',
+							section: 'Dependencies'
+						}
+					]
+				}
+			}
+		],
+		[
+			'@semantic-release/changelog',
+			{
+				changelogTitle: '# Changelog'
+			}
+		]
+	],
+	branches: ['main'],
+	preset: 'conventionalcommits'
+};
--- package.release.config.js
+++ package.release.config.js
@@ -1,16 +1,16 @@
 // package.release.config.js
-const common = require("./common.release.config.js");
+const common = require('./common.release.config.js');
 
 module.exports = {
-  ...common,
-  plugins: [
-    ...common.plugins,
-    [
-      "semantic-release-vsce",
-      {
-        packageVsix: true,
-      },
-    ],
-    "semantic-release-stop-before-publish",
-  ],
+	...common,
+	plugins: [
+		...common.plugins,
+		[
+			'semantic-release-vsce',
+			{
+				packageVsix: true
+			}
+		],
+		'semantic-release-stop-before-publish'
+	]
 };

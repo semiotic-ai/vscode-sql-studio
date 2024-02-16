fca28852668495baa3e54ff132f51c1577d6c197
Tümay Tuzcu <tumay@semiotic.ai>
2024-02-16 22:16:24 +0300

    fix(graphtables): Replicated graph-node snake case
    Signed-off-by: Tümay Tuzcu <tumay@semiotic.ai>

--- src/graphtables/snake.ts
+++ src/graphtables/snake.ts
@@ -1,23 +1,22 @@
-// Regexps involved with splitting words in various case formats.
-const LOWER_UPPER_RE = /(\p{Ll})(\p{Lu})/gu;
-const UPPER_LOWER_RE = /(\p{N})(\P{L})/gu;
+/** This regex is derived from graph-node snake case library which has realy strange
+ * results. See [v0.11.4 of inflector package](https://github.com/whatisinternet/Inflector/blob/master/src/cases/snakecase/mod.rs)
+ **/
+const GRAPH_SNAKE_CASE_RE =
+	/((?<=\p{Ll})\p{Lu}|(?<=\p{Ll})\p{N}|(?<=\p{N})\p{Lu}\p{Ll}|\p{N}\p{Ll})/gu;
 
 // Regexp involved with stripping non-word characters from the result.
-const DEFAULT_STRIP_REGEXP = /[^\p{L}\p{N}\s]+/giu;
+const DEFAULT_STRIP_REGEXP = /[^\p{L}\p{N}_]+/giu;
 // The replacement value for splits.
-const SPLIT_REPLACE_VALUE = '$1\0$2';
+const SPLIT_REPLACE_VALUE = '_$1';
 
 /**
  * Convert a string to snake case (`foo_bar`).
  */
 export function snakeCase(value: string): string {
-	const words = value
-		.replace(LOWER_UPPER_RE, SPLIT_REPLACE_VALUE)
-		.replace(UPPER_LOWER_RE, SPLIT_REPLACE_VALUE)
-		//.replace(DEFAULT_STRIP_REGEXP, ' ')
-		.split(/\s/g)
-		.map((v) => v.trim())
-		.filter((v) => v.length > 0);
+	const result = value
+		.replace(GRAPH_SNAKE_CASE_RE, SPLIT_REPLACE_VALUE)
+		.replace(DEFAULT_STRIP_REGEXP, '_')
+		.toLowerCase();
 
-	return words.map((input) => input.toLowerCase()).join('_');
+	return result;
 }

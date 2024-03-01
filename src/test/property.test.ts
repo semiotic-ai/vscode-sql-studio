0d070713b7243e773debb259307ba59dbaff7d9d
Anirudh <anirudh@semiotic.ai>
2024-03-01 08:13:44 -0800

    test: remove test teardowns

--- src/test/property.test.ts
+++ src/test/property.test.ts
@@ -7,13 +7,8 @@ import {
 	getPropertyLineNumber
 } from '../editor/property';
 import * as path from 'path';
-import { teardown } from 'mocha';
 
 suite('getPropertyValue', () => {
-	teardown(async () => {
-		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-	});
-
 	test('It should return an empty string if the property is not in the doc', async () => {
 		// Open the document you want to test
 		const uri = vscode.Uri.file(
@@ -25,6 +20,7 @@ suite('getPropertyValue', () => {
 		const property = 'foo';
 		const value = getPropertyValue(property, document);
 		assert.strictEqual(value, '');
+		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
 	});
 
 	test('It should return the correct value for a property in the file', async () => {
@@ -38,14 +34,11 @@ suite('getPropertyValue', () => {
 		const property = '--+ID: ';
 		const value = getPropertyValue(property, document);
 		assert.strictEqual(value, 'HUZDsRpEVP2AvzDCyzDHtdc64dyDxx8FQjzsmqSg4H3B');
+		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
 	});
 });
 
 suite('addPropertyToEditor', () => {
-	teardown(async () => {
-		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-	});
-
 	test('It should not add a property that is in the document', async () => {
 		const uri = vscode.Uri.file(
 			path.join(__dirname, '..', '..', 'src', 'test', 'samples', 'query-with-properties.gsql')
@@ -63,6 +56,7 @@ suite('addPropertyToEditor', () => {
 
 		const newValue = getPropertyValue(property, editor.document);
 		assert.strictEqual(newValue, 'HUZDsRpEVP2AvzDCyzDHtdc64dyDxx8FQjzsmqSg4H3B');
+		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
 	});
 
 	test('It should add a property that is not in the document', async () => {
@@ -82,14 +76,11 @@ suite('addPropertyToEditor', () => {
 
 		const newLineNumber = getPropertyLineNumber(property, editor.document);
 		assert.strictEqual(newLineNumber, 1);
+		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
 	});
 });
 
 suite('replacePropertyInEditor', () => {
-	teardown(async () => {
-		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-	});
-
 	test('It should add a property if it is not in the document', async () => {
 		const uri = vscode.Uri.file(
 			path.join(__dirname, '..', '..', 'src', 'test', 'samples', 'query-with-properties.gsql')
@@ -109,6 +100,7 @@ suite('replacePropertyInEditor', () => {
 
 		const idLineNumber = getPropertyLineNumber('--+ID: ', editor.document);
 		assert.strictEqual(idLineNumber, 2);
+		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
 	});
 
 	test('If a property is in the document, replace its value', async () => {
@@ -128,5 +120,6 @@ suite('replacePropertyInEditor', () => {
 
 		const newValue = getPropertyValue(property, editor.document);
 		assert.strictEqual(newValue, '12345');
+		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
 	});
 });
